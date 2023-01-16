cb5fafdb68fcc9ece15df6c43d2c958b1ecb8fd8
Hynn <choihwoong@me.com>
2023-01-16 16:33:46 +0900

    fix : fixed config

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ const config = {
             password : 'Apple1984!',
             database : 'boardsys',
             port : '3306',
+            host : "127.0.0.1",
             dialect : 'mysql',
             define : {
                 freezeTableName : true,
